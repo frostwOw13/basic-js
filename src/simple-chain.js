2189bd2e691de9e056b3d82e9eedc60da1e28eae
frostwOw13 <frostwow13@yandex.ru>
2021-03-02 19:20:53 +0300

    feat: complete chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,44 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-
-  error: false,
   chain: [],
-
-  getLength() {
-    this.chain.length;
-  },
+  
+  getLength () {
+      this.chain.length;
+   },
 
   addLink(value) {
-    value === undefined ? this.chain.push(`( )`) : this.chain.push(`( ${value} )`);
-        return this;
+      value === undefined ? this.chain.push(`( )`) : this.chain.push(`( ${value} )`);
+      return this;
   },
 
-  removeLink(position) {
-    if (position < 1 || position > this.chain.length || position === NaN || position % 1 !== 0) {
-      this.error = true;
-      throw new Error('Error');
-  } else {
-      this.chain.splice(position - 1, 1);
-  }
-  return this;
+  removeLink (position) {
+      if (position < 1 || position > this.chain.length || !Number.isInteger(position)) {
+          this.chain = [];
+          throw new Error('Error');
+      } else {
+          this.chain.splice(position - 1, 1);
+          return this;
+      }
+      
   },
 
-  reverseChain() {
+  reverseChain () {
     this.chain.reverse();
-        return this
+    return this;
   },
 
-  finishChain() {
-    if (this.error === false) {
-      let resultChain = this.chain;
-      this.chain = [];
-      return resultChain.join('~~');
-    } else {
-        this.chain = [];
-     }
+  finishChain () {
+         let resultChain = this.chain;
+         this.chain = [];
+         return resultChain.join('~~');
   }
 };
-
 
 module.exports = chainMaker;
